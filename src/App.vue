9c8f20cf78a39d25f7c89f9f3b6d5219a19a8f84
Hiro Nakano <84900813+Rayoo-Nakano@users.noreply.github.com>
2023-02-14 21:18:28 +0900

    v3.5

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
-  <main>
-    <header>
-      <h2>Amplify × Vue × Vite </h2>
-      <nav class="nav">
-        <ul class="list">
-          <li><router-link class="routerLink" to="/">Home</router-link></li>
-          <li><router-link class="routerLink" to="/chat">Chat</router-link></li>
-          <li><router-link class="routerLink" to="/ai">AI</router-link></li>
-          <li><router-link class="routerLink" to="/geo">Geo</router-link></li>
-        </ul>
-      </nav>
-      <button @click="signOut" class="contact">Sign out</button>
-    </header>
-    <section>
-      <router-view/>
-    </section>
-  </main>
+  <!-- 追加 1 -->
+  <authenticator>
+      <main>
+        <header>
+          <h2>Amplify × Vue × Vite</h2>
+          <nav class="nav">
+            <ul class="list">
+              <li><router-link class="routerLink" to="/">Home</router-link></li>
+              <li><router-link class="routerLink" to="/chat">Chat</router-link></li>
+              <li><router-link class="routerLink" to="/ai">AI</router-link></li>
+              <li><router-link class="routerLink" to="/geo">Geo</router-link></li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          <router-view/>
+        </section>
+      </main>
+  <!-- 追加 2 -->
+  </authenticator>
 </template>
 
 <style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,41 @@
 <template>
-    <h1>Home View</h1>
-</template>
+    <div class="home">
+      <h1>
+        Welcome to Amplify Vue Workshop, {{ username }} ! Click
+        <a href="https://catalog.workshops.aws/amplify-vue3/ja-JP"> here </a> for hands-on.
+      </h1>
+      <img src="https://d15nimfww918g4.cloudfront.net/img.png" />
+      <p>
+        かの Larry Wall は、プログラマの三大美徳として怠惰（Laziness）、短気（Impatence）、傲慢（Hubis）を挙げました。
+        ここで「怠惰である」とは、例えば効率化や自動化によって面倒なことを何度もしないで済むようにし、開発の効率を上げることを意味します。
+      </p>
+      <p>
+        AWS では、AWS を使用したスケーラブルなモバイルアプリおよびウェブアプリの作成、設定、実装を容易にする AWS Amplify
+        を提供しています。このアプリは AWS Amplify を使って作成したサンプルアプリとなります。
+      </p>
+      <h3>参考リンク</h3>
+      <ul>
+        <li>
+          <a href="https://aws.amazon.com/jp/builders-flash/202203/amplify-studio-flash-development/?awsf.filter-name=*all"> アプリを爆速で開発 ! AWS Amplify Studio で出来ること。</a>
+        </li>
+        <li>
+          <a href="https://docs.amplify.aws/"> AWS Amplify 公式ドキュメント </a>
+        </li>
+        <li>
+          <a href="https://amplify-sns.workshop.aws/ja/"> Amplify SNS Workshop </a>
+        </li>
+        <li>
+          <a href="https://github.com/aws-amplify-jp/awesome-aws-amplify-ja">AWS Amplify のツール、チュートリアル、記事などのリンク集</a>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      username: String,
+    },
+  };
+  </script>
   
